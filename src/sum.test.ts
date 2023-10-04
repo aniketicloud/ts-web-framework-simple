d34f0686b150e36ca569add85743ac4f38bece89
Aniket Pandharabale <aniketicloud@gmail.com>
2023-10-04 20:35:28 +0530

    adding msw for node environment for test cases

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,6 +1,25 @@
 import { expect, test } from "vitest";
 import { sum } from "./sum";
+import { server } from "./mocks/server";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("fetching actual data", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await response.json();
+  console.log("first:", data);
+  expect(data).toBeDefined();
+  expect(data.title).toBe("delectus aut autem");
+});
+
+test("fetching msw data", async () => {
+  server.listen({ onUnhandledRequest: "error" });
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await response.json();
+  console.log("second:", data);
+  expect(data).toBeDefined();
+  expect(data.title).toBe("my changed title");
+  server.close();
 });
